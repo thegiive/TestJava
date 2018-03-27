3e1085ab51a8f03ffc1dd5fad396f43696d7bb3a
Wisely Chen <thegiive@gmail.com>
2018-03-27 22:28:53 +0800

    Still got 2 test case fail.

--- CheckBST.java
+++ CheckBST.java
@@ -4,31 +4,51 @@ class Node {
     Node right;
  }
 public class CheckBST{
-    boolean checkBST(Node root) {
+    public boolean checkBST(Node root) {
         if(root == null ) return true ; 
         if((root.left == null) && (root.right == null)) return true ; 
+        
+        boolean result = false ; 
         if((checkBST(root.right) && (checkBST(root.left)))){
-            if((root.right != null) && ( root.data < root.right.data ) ){
-                if((root.left != null) && ( root.data > root.left.data ) ){
-                    return true ; 
-                }                    
-            }                    
+            if(root.right != null){
+                if(root.left != null){
+                    if((root.data < findMinValue(root.right)) && (root.data > findMaxValue(root.left))){                    
+                        result = true; 
+                    }
+                }else{
+                    if(root.data < findMinValue(root.right)){
+                        result = true ; 
+                    }            
+                }
+            }else{
+                if(root.left != null){
+                    if(root.data > findMaxValue(root.left)){                    
+                        result = true; 
+                    }
+                }
+            }
         }
-        return false ; 
-        // if(root == null ) return true ; 
-        // if((root.left == null) && (root.right == null)) return true ; 
-        // if((checkBST(root.right) && (checkBST(root.left)))){
-        //     if(root.right != null){
+        return result ; 
+    }
 
-        //     }else{
+    public int findMinValue(Node root){
+        if(root.left !=null){
+            return findMaxValue(root.left); 
+        }else{
+            return root.data ; 
+        }
+        
+    }
 
-        //     }
-        //     && ( root.data < root.right.data ) ){
-        //         if((root.left != null) && ( root.data > root.left.data ) ){
-        //             return true ; 
-        //         }                    
-        //     }                    
-        // }
-        // return false ; 
+    public int findMaxValue(Node root){
+        if(root.right !=null){
+            return findMaxValue(root.right); 
+        }else{
+            return root.data ; 
+        }
+    }
+
+    public static void main(String[] args){
+
     }    
 }

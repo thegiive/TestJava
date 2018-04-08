1bedbd503a0ee40b2c3c4e8a6f1081ff7e59a6fd
Wisely Chen <thegiive@gmail.com>
2018-04-08 08:43:22 +0800

    Solved

--- sql/revising-the-select-query.sql
+++ sql/revising-the-select-query.sql
@@ -1,1 +1,11 @@
+
 select * from CITY where COUNTRYCODE='USA' and POPULATION > 100000 
+
+-- Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA.
+
+select name from CITY where COUNTRYCODE='USA' and POPULATION > 120000 ;
+
+-- Query all columns (attributes) for every row in the CITY table.
+
+
+select * from CITY; 

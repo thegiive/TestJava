fb686f9fc0e8a66ba364de4bdfd5dd412218d1d5
Wisely Chen <thegiive@gmail.com>
2018-04-14 08:35:26 +0800

    solved

--- sql/select_longest_city.sql
+++ sql/select_longest_city.sql
@@ -2,3 +2,8 @@
 
 SELECT CITY,length(CITY) FROM STATION ORDER BY length(CITY) DESC,CITY DESC LIMIT 1;
 SELECT CITY,length(CITY) FROM STATION ORDER BY length(CITY) ASC,CITY ASC LIMIT 1;
+
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY , 1) in ('a','e','i','o','u') ;
+
+

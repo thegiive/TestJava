3853eda2703f25a006f151411a2e38b70d97db0b
Wisely Chen <thegiive@gmail.com>
2018-04-20 09:45:06 +0800

    Solved

--- sql/select_longest_city.sql
+++ sql/select_longest_city.sql
@@ -8,3 +8,15 @@ SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY , 1) in ('a','e','i','o','u') 
 -- Query the list of CITY names ending with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY,1) in ('a','e','i','o','u')
 
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY,1) in ('a','e','i','o','u') AND LEFT(CITY , 1) in ('a','e','i','o','u'); 
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY , 1) not in ('a','e','i','o','u'); 
+
+-- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY,1) not in ('a','e','i','o','u');
+
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY,1) not in ('a','e','i','o','u') and LEFT(CITY , 1) not in ('a','e','i','o','u'); 
+

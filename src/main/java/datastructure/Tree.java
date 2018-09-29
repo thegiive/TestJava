df87cf9e9f22c7c6d9a28642b8734d3fb7cdfb92
Wisely Chen <thegiive@gmail.com>
2018-09-29 15:32:23 +0800

    Add

---
+++ src/main/java/datastructure/Tree.java
@@ -0,0 +1,59 @@
+package datastructure;
+
+
+import java.util.Scanner;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class Tree {
+
+
+    public static String preOrder(Node root) {
+        if(root == null) return "";
+        String result = "";
+        result += root.data+ " ";
+        if(root.left != null) result+= preOrder(root.left);
+        if(root.right!= null) result+= preOrder(root.right);
+        return  result ;
+
+    }
+
+    public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        preOrder(root);
+    }
+
+}
---
+++ src/test/java/datastructure/TreeTest.java
@@ -0,0 +1,22 @@
+package datastructure;
+
+import junit.framework.TestCase;
+
+public class TreeTest extends TestCase {
+
+    public void testPreOrder() {
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+
+        n3.left = n2 ;
+        n2.left = n1 ;
+        n3.right = n4 ;
+        n4.right = n5 ;
+
+        assertEquals("3 2 1 4 5 " , Tree.preOrder(n3));
+
+    }
+}
